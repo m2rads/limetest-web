00c3c78af7a2d12f344639d6f8e3c14c7fa20f74
Mohammad Rad <argo.mohrad@gmail.com>
2025-05-11 14:26:29 -0700

    Add dashboard layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from 'react'
+import { SidebarProvider } from "@/components/ui/sidebar"
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      {children}
+    </SidebarProvider>
+  )
+} 
